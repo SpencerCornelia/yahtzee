c93e581b86f76abc9d3825863a8bda34fbb716d2
Spencer Cornelia <cornelia.spencer@yahoo.com>
2017-03-19 18:34:48 -0700

    added functionality for first and second dice roll

--- Assignment5/Yahtzee.java
+++ Assignment5/Yahtzee.java
@@ -7,6 +7,7 @@
 import acm.io.*;
 import acm.program.*;
 import acm.util.*;
+import java.util.*;
 
 public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 	
@@ -26,11 +27,56 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 	}
 
 	private void playGame() {
-		/* You fill this in */
+		int numOfTurns = N_SCORING_CATEGORIES;
+		while (numOfTurns > 0) {
+			String currentPlayerTurn = determinePlayerTurn(playerNames);
+			display.waitForPlayerToClickRoll(playerCounter);
+			createDice(diceArray);
+			display.displayDice(diceArray);
+			display.waitForPlayerToSelectDice();
+			diceRoll();
+			display.waitForPlayerToSelectDice();
+			
+			playerCounter++;
+			numOfTurns --;
+		}
 	}
+	
+	private void createDice(int[] diceArray) {
+		for (int i = 0; i < diceArray.length; i++) {
+			diceArray[i] = rgen.nextInt(1, 6);
+		}
+	}
+	
+	private String determinePlayerTurn(String[] playerNames) {
+		if (playerNames.length == 1) {
+			return playerNames[0];
+		}
+		
+		if (playerCounter == playerNames.length + 1) {
+			playerCounter = 0;
+			return playerNames[playerCounter];
+		}
+		return playerNames[playerCounter - 1];
+	}
+	
+	private void diceRoll() {
+		for (int i = 0; i < diceArray.length; i++) {
+			if (display.isDieSelected(i)) {
+				updatedDiceArray[i] = rgen.nextInt(1, 6);
+			} else {
+				updatedDiceArray[i] = diceArray[i];
+			}
+		}
+		display.displayDice(updatedDiceArray);
+	}
+	
 		
 /* Private instance variables */
 	private int nPlayers;
+	private int[] diceArray = new int[N_DICE];
+	private int[] updatedDiceArray = new int[N_DICE];
+	private int playerCounter = 1;
 	private String[] playerNames;
 	private YahtzeeDisplay display;
 	private RandomGenerator rgen = new RandomGenerator();
